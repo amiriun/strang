95fe1cfbef867cbc05629004ce671c78fa7a6540
Amir <amircup2006@gmail.com>
2019-06-20 22:31:53 +0430

    Add first helper which name is random
    with random helper you can create random strings and specify length and characters of generated string

--- src/Strang.php
+++ src/Strang.php
@@ -9,4 +9,22 @@ namespace Amiriun;
  */
 class Strang
 {
+    /**
+     * @param $length
+     * @param string|null $characters
+     * @return string
+     */
+    public function random($length, $characters = null)
+    {
+        if (is_null($characters)) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
 }
